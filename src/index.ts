30cda0b9157f22f44a5a34d721be499853491a4f
juanlramos <juanl.bnr@gmail.com>
2025-08-04 14:24:18 -0300

    fix: ajustado problemas em produção

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
+import { Knex } from "./server/database/knex";
 import { server } from "./server/Server";
 
-server.listen(process.env.PORT || 3333, () => {
-  console.log("Servidor rodando na porta", process.env.PORT || 3333);
-});
+const startServer = () => {
+  server.listen(process.env.PORT || 3333, () => {
+    console.log("Servidor rodando na porta", process.env.PORT || 3333);
+  });
+};
+
+
+//configuração, para em produção rodar as migrations automaticas, mas em localhost so rodar o server
+if (process.env.IS_LOCALHOST !== "true") {
+  Knex.migrate
+    .latest()
+    .then(() => {
+      startServer();
+    })
+    .catch(console.log);
+} else {
+  startServer();
+}
