87ee387dd1cc424959e4b73a05b8cfacac7880de
juanlramos <juanl.bnr@gmail.com>
2025-08-08 16:25:52 -0300

    feat: adicionado configurações de ambiente para produção

--- src/server/database/knex/index.ts
+++ src/server/database/knex/index.ts
@@ -1,5 +1,18 @@
 import knex from "knex";
+import "dotenv/config";
+import pg from "pg";
 import { development, production, test } from "./Environment";
+
+if (process.env.NODE_ENV === "production") {
+  //tratamento para os dados serem tratados como inteiro
+  pg.types.setTypeParser(20, "text", parseInt);
+
+  /**
+   * caso outro dado esteja vindo como string e não deveria
+   * CTRL + CLICK no setTypeParser
+   * CTRL + CLICK no typeId e verifica o codigo do tipo (ex: BOOL = 16)
+   */
+}
 
 const getEnvironment = () => {
   switch (process.env.NODE_ENV) {
