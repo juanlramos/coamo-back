f950ae7b6316734cb2b46b6b90e97237d7fc794c
juanlramos <juanl.bnr@gmail.com>
2025-08-09 13:54:18 -0300

    feat: configurado o CORS

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,12 +1,19 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 import "./shared/services/TranslationsYup";
 import { router } from "./routes";
 
 const server = express();
 
-//aqui a gente informa que o servidor vai usar nossas rotas
+server.use(
+  cors({
+    origin: process.env.ENABLED_CORS?.split(";") || [], //como o enabled_cors pode ser undefined
+  })
+); //assim libera o CORS para o frontend, passando a variavel ambiente com a lista de url dos fronts
 server.use(express.json()); // para permitir o uso de JSON no corpo das requisições
+
+//aqui a gente informa que o servidor vai usar nossas rotas
 server.use(router);
 
 export { server };
